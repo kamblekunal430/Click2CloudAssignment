d371cc7f81502466f0f1ab8566dc0c3f5806598c
Kunal Kamble <kunal.kamble@simplilearn.net>
2022-09-14 00:34:02 +0530

    created sever and fetched data from api

---
+++ backend/src/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const axios = require("axios");
+const config = require("config");
+const PORT = config.get("PORT");
+const url = config.get("COVID_DATA_URL");
+
+const app = express();
+
+
+app.get("/", function (req, res) {
+  axios
+    .get(url)
+    .then(function (data) {
+      res.send({
+        status: data.status,
+        data: data.data,
+      });
+    })
+    .catch(function (err) {
+      res.send({
+        status: err.response.status,
+        data: err.message,
+      });
+    });
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on localhost:", PORT);
+});
